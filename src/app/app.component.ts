0710d6752e24547839c19289bc9bccd03903ac61
Cosmin-Catalin Crisan <cosmin.crisan@cloverheap.com>
2021-06-10 20:26:33 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { HelloWorldService } from './hello-world.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'medium-sample';
+	title: string = 'medium-sample';
+	text: string;
+
+	constructor(private _helloWorldService: HelloWorldService) { }
+
+	doAction(): void {
+		this.text = this._helloWorldService.getHelloWorldMessage();
+	}
 }
